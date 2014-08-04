483a2d7d01c34e0a4fc2c9836b2aa33c960c3504
RideZeBigOne <julien@X13>
2014-08-04 22:22:54 +0200

    suppression fichier passwd.h indesirable

--- serveur/emitter/passwd.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_PASSWD
-#define H_PASSWD
-   char *user = "root";
-    char *password = "mysql"; 
-    char *database = "domotique";
-	char *server = "localhost";
-    char query [400];
-#endif
