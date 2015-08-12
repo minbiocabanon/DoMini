955bd8ccb02c725eb9f8488355277e5178139cc3
MinBioCabanon <minbiocabanon@gmail.com>
2015-08-12 21:59:07 +0200

    ajout script pour verif connexion wifi + cron

---
+++ serveur/systeme/checkwifi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ping -c4 192.168.1.1 > /dev/null
+
+if [ $? != 0 ] 
+then
+  echo "No network connection, restarting wlan0"
+  /sbin/ifdown 'wlan0'
+  sleep 5
+  /sbin/ifup --force 'wlan0'
+fi
