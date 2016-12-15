8e542d1ed3e8b816238b23848abd2d21d6951320
MinBioCabanon <minbiocabanon@gmail.com>
2016-12-15 21:33:03 +0100

    correction bug dans testip.sh, remplacement ftp par curl

--- serveur/www/domini/script/testip.sh
+++ serveur/www/domini/script/testip.sh
@@ -4,21 +4,27 @@ installpath=/var/www/domini/script/
 . $installpath/myvar.sh
 
 # on se  place dans le répertoire de telechargement
-cd /tmp
+cd ~/download/
 mkdir cache_wget
 cd cache_wget
 
+#on ajoute la date courante en début de fichier
+date > myip.txt
+$'\n' >> myip.txt
+
 # on commence par créer un fichier contenant son @ IP publique
 echo recuperation adresse IP publique
-curl ipinfo.io/ip > myip.txt
+curl ipinfo.io/ip >> myip.txt
 
 # upload des fichiers sur le site distant
 echo upload du site ...
-lftp ftp://$LOGIN:$PASSWORD@ftpperso.free.fr -e "mirror -R /tmp/cache_wget/ /static_domini  ; quit"
+#lftp ftp://$LOGIN:$PASSWORD@ftpperso.free.fr -e "mirror -R /tmp/cache_wget/ /static_domini  ; quit"
+curl -T myip.txt ftp://ftpperso.free.fr/static_domini/ --user $LOGIN:$PASSWORD
 
 #suppression des fichiers temporaires
 echo supression des fichiers temporaires ...
-rm -f  /tmp/cache_wget/*
-rm -f -r /tmp/cache_wget/csv
-rm -f -r /tmp/cache_wget/css
-rm -f -r /tmp/cache_wget/webcam
+rm -f -r ~/download/cache_wget/
+# rm -f  ~/download/cache_wget/*
+# rm -f -r ~/download/cache_wget/csv
+# rm -f -r ~/download/tmp/cache_wget/css
+# rm -f -r ~/download/tmp/cache_wget/webcam
