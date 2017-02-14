1955da613bb61cc7b59d84c99ddde5513c1e688d
minbiocabanon <minbiocabanon@gmail.com>
2017-02-14 21:31:42 +0100

    correction bug dans le script check wifi

--- serveur/systeme/checkwifi.sh
+++ serveur/systeme/checkwifi.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-ping -c4 192.168.1.1 > /dev/null
+echo "pinging 192.168.0.1 ..."
+ping -c4 192.168.0.1 > /dev/null
 
 if [ $? != 0 ] 
 then
-  echo "No network connection, restarting wlan0"
+  echo "No network connection, stopping wlan0"
   /sbin/ifdown 'wlan0'
   sleep 5
+  echo "starting waln0"	
   /sbin/ifup --force 'wlan0'
+else
+  echo "waln0 is connected, nothing to do :-)"	
 fi
