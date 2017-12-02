e1dda9370c77a45c8a678d25e4b3a8303e84ea8d
minbiocabanon <minbiocabanon@gmail.com>
2017-12-02 20:57:54 +0100

    correction mineure

--- serveur/www/domini/script/webcam_10m.sh
+++ serveur/www/domini/script/webcam_10m.sh
@@ -2,15 +2,19 @@
 cd /var/www/domini/webcam/
 
 # capture l'image de la webcam, sortie sous le nom ext.jpg
+echo curl 192.168.0.116
 curl --url http://user:user@192.168.0.116/axis-cgi/jpg/image.cgi --output ./ext.jpg
 
 # capture l'image de la webcam SEJOUR, sortie sous le nom sejour.jpg
+echo curl 192.168.0.117
 curl --url http://user:user@192.168.0.117/axis-cgi/jpg/image.cgi --output ./porche.jpg
 
 # capture l'image de la webcam du garage sortie sous le nom garage.jpg
+echo curl 192.168.0.121
 curl --url "http://192.168.0.121:88/cgi-bin/CGIProxy.fcgi?usr=user&pwd=user&cmd=snapPicture2" --output ./garage_full.jpg
 
 # Capture Foscam C2 sejour
+echo curl 192.168.0.130
 curl --url "http://192.168.0.130:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=user&pwd=userFoscamC2" --output ./sejour_full.jpg
 
 
