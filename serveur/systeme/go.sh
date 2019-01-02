7442fa5c8160410db73225689ae19f0bb613f140
minbiocabanon <minbiocabanon@gmail.com>
2019-01-02 19:19:23 +0100

    ajout lancement daemon ups dans le go.sh , cela permet d'avoir la bdd rempli avec l'état de l'onduleur et les noticfications pushbullet

--- serveur/systeme/go.sh
+++ serveur/systeme/go.sh
@@ -3,10 +3,13 @@
 #on fixe le baudrate du port ttyUSB0 a 57600 bauds
 stty -F /dev/ttyUSB0 57600
 
-#on lance le soft
+#on lance les logiciels
+# lancement du daemon qui gère l'onduleur (obligatoire pour remplir la bdd et avoir les notifications pushbullet)
+python ~/src/domini/serveur/ups/ups_daemonized.py
+
 #python /home/julien/src/domini/seveur/pyreceiver/pyreceiver
 python ~/src/domini/serveur/pyReceiver/pyreceiver.py &
-#
+
 #on relance le serveur web (plante!?)
 sudo /etc/init.d/lighttpd restart
 sudo /etc/init.d/mysql restart
