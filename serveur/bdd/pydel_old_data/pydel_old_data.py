19b587ff3948fbe7584943a12d1977b4ebe8b1c9
minbiocabanon <minbiocabanon@gmail.com>
2021-01-16 17:38:25 +0100

    python script upgraded for python3

--- serveur/bdd/pydel_old_data/pydel_old_data.py
+++ serveur/bdd/pydel_old_data/pydel_old_data.py
@@ -3,7 +3,7 @@ import sys
 import syslog
 
 import time
-import MySQLdb as mdb
+import mariadb as mdb
 
 tables = ["chauffage_log", "voletroulant_log", "bypass_pc_log", "pellets_rsv", "ups", "internet_connex"]
 NB_JOUR = 180
@@ -14,36 +14,36 @@ def setup():
 	syslog.openlog("pydel_old_data")
 	syslog.syslog("Demarrage")
 	logmessage = " Demarrage pydel_old_data.py, Programme qui supprime les donnees d'une date selon un nombre de jours."
-	print logmessage
+	print(logmessage)
 	syslog.syslog(logmessage)
 	print ('End Setup')
 # -- fin setup --
 
 # -- get all about time and date --
 def get_time():
-	print '\nget_time()'
+	print('\nget_time()')
 	# display date/time
 	logmessage = time.strftime(" Date et heure : %Y-%m-%d %H:%M:%S")
-	print logmessage
+	print(logmessage)
 	syslog.syslog(logmessage)
 	# afficher le timestamp
 	logmessage = " Unixtime : " + str(time.time())
-	print logmessage
+	print(logmessage)
 	syslog.syslog(logmessage)
 # -- end get_time --
 
 # -- clean tables --
 def clean_table():
 	for table in tables:
-		print "\ntable : %s" % table
+		print("\ntable : %s" % table)
 		logmessage = "On supprime toutes les lignes de la table "+str(table)+" plus vieille de "+str(NB_JOUR) +" jour(s))"
-		print logmessage
+		print(logmessage)
 		syslog.syslog(logmessage)
 		# Preparation de la requete MySQL
 		# on ne supprime que les donnees de J-n
 		try:
 			# Open MySQL session
-			con = mdb.connect('localhost','root','mysql','domotique')
+			con = mdb.connect(user="root",password="mysql",host="localhost",database="domotique")
 			cur = con.cursor()
 			# prepare query
 			query = 'DELETE FROM `domotique`.`{0}` WHERE `date_time` < DATE_SUB(CURDATE(), interval {1} DAY);'.format(table, NB_JOUR)
@@ -52,14 +52,14 @@ def clean_table():
 			#result = cur.fetchone()
 			con.commit()
 			
-		except mdb.Error, e:
+		except mdb.Error as e:
 			# create variable
 			#result = 0
 			# Display MySQL errors
 			try:
-				print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+				print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
 			except IndexError:
-				print "MySQL Error: %s" % str(e)	
+				print("MySQL Error: %s" % str(e))	
 
 		finally:
 			# closing database connection.
@@ -69,11 +69,11 @@ def clean_table():
 			con.close()			
 				
 		logmessage = "Donnees de la table "+str(table)+" < "+str(NB_JOUR)+" jours supprimes ..."
-		print logmessage
+		print(logmessage)
 		syslog.syslog(logmessage)
 				
 	logmessage = "Fin pydel_old_data"
-	print logmessage
+	print(logmessage)
 	syslog.syslog(logmessage)
 
 # -- end clean_table --
@@ -83,10 +83,10 @@ def optimize():
 	for tabname in tables :
 		try:
 			logmessage = "Optimisation de la table "+str(tabname)+" en cours ..."
-			print logmessage
+			print(logmessage)
 			syslog.syslog(logmessage)
 			# Open MySQL session
-			con = mdb.connect('localhost','root','mysql','domotique')
+			con = mdb.connect(user="root",password="mysql",host="localhost",database="domotique")
 			cur = con.cursor()
 			# prepare query
 			query = 'OPTIMIZE TABLE `{0}`;'.format(tabname)
@@ -94,14 +94,14 @@ def optimize():
 			cur.execute(query)
 			#result = cur.fetchone()
 
-		except mdb.Error, e:
+		except mdb.Error as e:
 			# create variable
 			#result = 0
 			# Display MySQL errors
 			try:
-				print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+				print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
 			except IndexError:
-				print "MySQL Error: %s" % str(e)
+				print("MySQL Error: %s" % str(e))
 		finally:
 			# closing database connection.
 			# Close all cursors
