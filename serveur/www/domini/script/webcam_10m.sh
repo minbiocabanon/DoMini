66dd68a1d8fd4e1f60b58b3d1c88f377e94a6cbe
minbiocabanon <minbiocabanon@gmail.com>
2021-01-16 17:50:18 +0100

    remove accent

--- serveur/www/domini/script/webcam_10m.sh
+++ serveur/www/domini/script/webcam_10m.sh
@@ -5,22 +5,24 @@ cd /var/www/domini/webcam/
 #echo curl 192.168.0.116
 #curl --url http://user:user@192.168.0.116/axis-cgi/jpg/image.cgi --output ./kiwi.jpg
 
+
 # capture l'image du garage
 echo capture ffmpeg 192.168.0.119:554/live0.264
-/usr/local/bin/ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.119:554/live0.264' -f singlejpeg -vframes 1 garage.jpg
+ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.119:554/live0.264' -f singlejpeg -vframes 1 garage.jpg
 
 # capture l'image jardin/terrasse
 echo capture ffmpeg 192.168.0.120:554/live0.264
-/usr/local/bin/ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.120:554/live0.264' -f singlejpeg -vframes 1 ext.jpg
+ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.120:554/live0.264' -f singlejpeg -vframes 1 ext.jpg
 
 # capture l'image de la webcam du garage sortie sous le nom garage.jpg
 # echo curl 192.168.0.121
-curl --url "http://192.168.0.121:88/cgi-bin/CGIProxy.fcgi?usr=user&pwd=user&cmd=snapPicture2" --output ./garage_full.jpg
+#curl --url "http://192.168.0.121:88/cgi-bin/CGIProxy.fcgi?usr=user&pwd=user&cmd=snapPicture2" --output ./garage_full.jpg
+curl --url "http://192.168.0.121:88/cgi-bin/CGIProxy.fcgi?usr=user&pwd=user&cmd=snapPicture2" --output ./garage.jpg
 
 # capture l'image du porche
 echo capture ffmpeg 192.168.0.129:554/live0.264
 #curl --url http://user:user@192.168.0.117/axis-cgi/jpg/image.cgi --output ./porche.jpg
-/usr/local/bin/ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.129:554/live0.264' -f singlejpeg -vframes 1 porche.jpg
+ffmpeg -y -rtsp_transport tcp -i 'rtsp://192.168.0.129:554/live0.264' -f singlejpeg -vframes 1 porche.jpg
 
 
 # Capture Foscam C2 sejour
