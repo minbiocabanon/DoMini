330d868d1127e8c1234355793f246d90e0037b5e
minbiocabanon <minbiocabanon@gmail.com>
2021-01-16 17:36:22 +0100

    shell commands upgraded to be compatible with ub2004

--- serveur/systeme/checkwifi.sh
+++ serveur/systeme/checkwifi.sh
@@ -5,13 +5,13 @@ ping -c4 192.168.0.1 > /dev/null
 
 if [ $? != 0 ] 
 then
-  killall ifdown
-  killall ifup
-  echo "No network connection, stopping wlan0"
-  /sbin/ifdown 'wlan0'
-  sleep 5
-  echo "starting waln0"	
-  /sbin/ifup --force 'wlan0'
+	echo "No network connection, stopping wlan0"
+	sudo ip link set wlan0 down
+	sleep 5
+	echo "starting waln0"	
+	sudo ip link set wlan0 up
+	echo "udhcp request"	
+	dhclient wlan0
 else
   echo "wlan0 is connected, nothing to do :-)"	
 fi
