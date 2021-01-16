2919171dad515af448676204c56357377c52d164
minbiocabanon <minbiocabanon@gmail.com>
2021-01-16 17:50:07 +0100

    remove accent

--- serveur/www/domini/script/graph_2m.sh
+++ serveur/www/domini/script/graph_2m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/domini/php/
-# charge les données temps réelles dans une table temporaire -> permet d'accélerer l'affichage de la page d'accueil de l'interface domotique
+# charge les donnees temps reelles dans une table temporaire -> permet d'accelerer l'affichage de la page d'accueil de l'interface domotique
 curl 0.0.0.0:80/php/instant_data.php
 
 curl 0.0.0.0:80/php/test_internet.php
@@ -8,6 +8,6 @@ curl 0.0.0.0:80/php/test_internet.php
 #lancement des page PHP qui vont generer les graphs
 cd /var/www/domini/php/highstock/
 
-# Génére le CSV avec les info de connexion internet
+# Genere le CSV avec les info de connexion internet
 curl 0.0.0.0:80/php/highstock/csv_test_internet.php
 
