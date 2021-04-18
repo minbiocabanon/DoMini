0ec24bc3d3d928c0105d0f8fa968016cd29d9b99
minbiocabanon <minbiocabanon@gmail.com>
2021-04-18 12:58:25 +0200

    fix go.sh to able pyreceiver at startup after power outage

--- serveur/systeme/go.sh
+++ serveur/systeme/go.sh
@@ -5,14 +5,17 @@ stty -F /dev/ttyUSB0 57600
 
 #on lance les logiciels
 # lancement du daemon qui gère l'onduleur (obligatoire pour remplir la bdd et avoir les notifications pushbullet)
-python ~/src/domini/serveur/ups/ups_daemonized.py
+echo "Lancement de ups_daemonized.py"
+/usr/bin/python3 ~/src/domini/serveur/ups/ups_daemonized.py &
 
-#python /home/julien/src/domini/seveur/pyreceiver/pyreceiver
-python ~/src/domini/serveur/pyReceiver/pyreceiver.py &
+echo "Lancement de pyreceiver.py"
+##la commande suivante permet de donner accés en lecture/écriture à tous les groupes
+sudo chmod 666 /dev/ttyUSB0
+/usr/bin/python3 ~/src/domini/serveur/pyReceiver/pyreceiver.py &
 
 #on relance le serveur web (plante!?)
-sudo /etc/init.d/lighttpd restart
-sudo /etc/init.d/mysql restart
+#sudo /etc/init.d/lighttpd restart
+#sudo /etc/init.d/mysql restart
 
 #lancement service pour ddns
-sudo /usr/local/bin/noip2
+#sudo /usr/local/bin/noip2
